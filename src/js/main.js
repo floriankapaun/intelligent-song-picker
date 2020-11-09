c241c262c2d6200860829689996775537254511b
Florian Kapaun <hello@florian-kapaun.de>
2020-11-09 08:50:19 +0100

    :sparkles: create class FaceLandmarksDetection

--- src/js/main.js
+++ src/js/main.js
@@ -1,84 +1,100 @@
-// import '../css/main.css';
-
 const faceLandmarksDetection = import(/* webpackPreload: true */ '@tensorflow-models/face-landmarks-detection');
 const tfjsBackendWebgl = import(/* webpackPreload: true */ '@tensorflow/tfjs-backend-webgl');
 const tf = import(/* webpackPreload: true */ '@tensorflow/tfjs-core');
 
-const VIDEO_SIZE = 500;
+class FaceLandmarksDetection {
+    constructor(input, output) {
+        this.VIDEO_WIDTH = 500;
+        this.VIDEO_HEIGHT = this.VIDEO_WIDTH;
+        this.TF_BACKEND = 'webgl';
+        this.input = input;
+        this.output = output;
+        this.model = undefined;
+        this.ctx = undefined;
+    }
+
+    async init() {
+        await tfjsBackendWebgl;
+        await (await tf).setBackend(this.TF_BACKEND);
+        // Start webcam
+        await this.setupCamera();
+        // Play video
+        this.input.play();
+        this.input.width = this.VIDEO_WIDTH;
+        this.input.height = this.VIDEO_HEIGHT;
+        // Style canvas
+        this.output.width = this.VIDEO_WIDTH;
+        this.output.height = this.VIDEO_HEIGHT;
+        // NECESSARY?
+        // const canvasContainer = document.querySelector('.canvas-wrapper');
+        // canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+        // Setup context
+        this.ctx = this.output.getContext('2d');
+        this.ctx.translate(this.output.width, 0);
+        this.ctx.scale(-1, 1);
+        this.ctx.fillStyle = '#32EEDB';
+        // Setup tensorflow.js faceLandmarksDetection model
+        this.model = await (await faceLandmarksDetection).load(
+            (await faceLandmarksDetection).SupportedPackages.mediapipeFacemesh,
+        );
+        // Start making predictions
+        this.renderPrediction();
+    }
+
+    async setupCamera() {
+        // Create new stream from camera
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                facingMode: 'user',
+                width: this.VIDEO_WIDTH,
+                height: this.VIDEO_HEIGHT,
+            },
+        });
+        // Set input source to created stream
+        this.input.srcObject = stream;
+        // Return promise that resolves if video is loaded
+        return new Promise((resolve) => {
+            this.input.onloadedmetadata = () => {
+                resolve(this.input);
+            };
+        });
+    }
+
+    async renderPrediction() {
+        // Make a prediction about the face landmarks
+        // the first prediction is taking very long.
+        const predictions = await this.model.estimateFaces({
+            input: this.input,
+        });
+        // Plott the input video as canvas background
+        // Clip the image and position the clipped part on the canvas
+        // ctx.drawImage(img,sx,sy,swidth,sheight,x,y,width,height)
+        this.ctx.drawImage(
+            this.input,
+            0, 0, this.VIDEO_WIDTH, this.VIDEO_HEIGHT,
+            0, 0, this.output.width, this.output.height,
+        );
+        // Plott the prediction onto the context
+        if (predictions.length > 0) {
+            for (const prediction of predictions) {
+                const keypoints = prediction.scaledMesh;
+                for (const keypoint of keypoints) {
+                    const [x, y] = keypoint;
+                    this.ctx.beginPath();
+                    this.ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
+                    this.ctx.fill();
+                }
+            }
+        }
+        // Render the next prediction
+        requestAnimationFrame(() => this.renderPrediction());
+    }
+}
 
 const video = document.getElementById('video');
 const canvas = document.getElementById('output');
-const canvasContainer = document.querySelector('.canvas-wrapper');
 
-let model, ctx, videoWidth, videoHeight;
+const fld = new FaceLandmarksDetection(video, canvas);
 
-async function setupCamera() {
-
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            facingMode: 'user',
-            // width: VIDEO_SIZE,
-            // height: VIDEO_SIZE
-        },
-    });
-    video.srcObject = stream;
-
-    return new Promise((resolve) => {
-        video.onloadedmetadata = () => {
-            resolve(video);
-        };
-    });
-}
-
-async function renderPrediction() {
-    // the first prediction is taking very long.
-    const predictions = await model.estimateFaces({
-        input: video,
-    });
-
-    ctx.drawImage(
-        video, 0, 0, videoWidth, videoHeight, 0, 0, canvas.width, canvas.height,
-    );
-
-    if (predictions.length > 0) {
-        for (let i = 0; i < predictions.length; i++) {
-            const keypoints = predictions[i].scaledMesh;
-            for (let j = 0; j < keypoints.length; j++) {
-                const [x, y] = keypoints[j];
-                ctx.beginPath();
-                ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
-                ctx.fill();
-            }
-        }
-    }
-
-    requestAnimationFrame(renderPrediction);
-};
-
-async function main() {
-    await tfjsBackendWebgl;
-    await (await tf).setBackend('webgl');
-    await setupCamera();
-    video.play();
-    videoWidth = video.videoWidth;
-    videoHeight = video.videoHeight;
-    video.width = videoWidth;
-    video.height = videoHeight;
-
-    canvas.width = videoWidth;
-    canvas.height = videoHeight;
-    canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
-
-    ctx = canvas.getContext('2d');
-    ctx.translate(canvas.width, 0);
-    ctx.scale(-1, 1);
-    ctx.fillStyle = '#32EEDB';
-
-    model = await (await faceLandmarksDetection).load(
-        (await faceLandmarksDetection).SupportedPackages.mediapipeFacemesh,
-    );
-    renderPrediction();
-};
-
-main();
+fld.init();
