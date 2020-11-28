b14ed8f8d8cdf18e20593b20557e0fb6d97a5694
Florian Kapaun <hello@florian-kapaun.de>
2020-11-28 18:35:29 +0100

    :building_construction: moved html content into views

--- client/src/views/App.vue
+++ client/src/views/App.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>App Screen</div>
+    <h2 id="greeting">Hey.</h2>
+    <!-- <p><button type="button" id="refresh-token">Refresh Token</button></p> -->
+    <div class="canvas-wrapper">
+        <canvas id="output"></canvas>
+        <video id="video" playsinline></video>
+    </div>
 </template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="home">
-    <!-- <img class="logo" src="../assets/logo.png" alt="logo" /> -->
-
-    <h1>👋Hello world🌎</h1>
+    <section class="max-width text-center">
+        <h1>{{ title }}</h1>
+        <h2>Let your mimics and emotions control Spotify.</h2>
+        <a href="/login" class="inline-block no-decoration">
+            <button type="button" tabindex="-1">
+                <svg class="icon-spotify" xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128" fill="#ffffff"><path d="M64,0C28.8,0,0,28.8,0,64s28.8,64,64,64 s64-28.8,64-64S99.52,0,64,0 M93.44,92.48c-1.28,1.92-3.52,2.56-5.44,1.28c-15.04-9.28-33.92-11.2-56.32-6.08 c-2.24,0.64-4.16-0.96-4.8-2.88c-0.64-2.24,0.96-4.16,2.88-4.8c24.32-5.44,45.44-3.2,62.08,7.04C94.08,88,94.4,90.56,93.44,92.48  M101.12,74.88c-1.6,2.24-4.48,3.2-6.72,1.6c-17.28-10.56-43.52-13.76-63.68-7.36c-2.56,0.64-5.44-0.64-6.08-3.2 c-0.64-2.56,0.64-5.44,3.2-6.08c23.36-7.04,52.16-3.52,72,8.64C101.76,69.44,102.72,72.64,101.12,74.88 M101.76,56.96 C81.28,44.8,47.04,43.52,27.52,49.6c-3.2,0.96-6.4-0.96-7.36-3.84c-0.96-3.2,0.96-6.4,3.84-7.36c22.72-6.72,60.16-5.44,83.84,8.64 c2.88,1.6,3.84,5.44,2.24,8.32C108.48,57.6,104.64,58.56,101.76,56.96"/></svg>
+                Connect with Spotify
+            </button>
+        </a>
+        <p class="fine-print">Premium required</p>
+    </section>
   </div>
 </template>
