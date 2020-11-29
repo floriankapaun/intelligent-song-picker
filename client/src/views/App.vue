60fb60b98f4b7b53464e0106d70ec7e575cbf80b
Florian Kapaun <hello@florian-kapaun.de>
2020-11-29 17:36:47 +0100

    :sparkles: implemented play/pause functions

--- client/src/views/App.vue
+++ client/src/views/App.vue
@@ -1,5 +1,7 @@
 <template>
     <h2 id="greeting">Hey {{ userName }}.</h2>
+    <button type="button" @click="startPlayback">play</button>
+    <button type="button" @click="stopPlayback">pause</button>
     <p><button type="button" @click="refreshAccessToken()">Refresh Token</button></p>
     <div class="canvas-wrapper">
         <canvas id="output"></canvas>
@@ -21,11 +23,18 @@ export default {
     },
     computed: {
         userName() {
-            console.log(spotifyController);
-            return spotifyController.user && spotifyController.user.name ? spotifyController.user.name : null;
+            return spotifyController.user && spotifyController.user.display_name
+                ? spotifyController.user.display_name
+                : null;
         }
     },
     methods: {
+        startPlayback() {
+            spotifyController.play();
+        },
+        stopPlayback() {
+            spotifyController.pause();
+        },
         refreshAccessToken() {
             spotifyController.refreshAccessToken();
         },
