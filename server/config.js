73b6a2876684759ef6fe7226f7758e8c2868e3d5
Florian Kapaun <hello@florian-kapaun.de>
2021-01-20 15:11:14 +0100

    :bug: updated server production settings

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const API_ROUTE = process.env.API_ROUTE;
 export const REDIRECT_URI = process.env.PRODUCTION
-    ? `https://spotify.kapaun.uber.space${API_ROUTE}/callback`
+    ? `https://intelligent-song-picker.com${API_ROUTE}/callback`
     : `http://localhost:${PORT}${API_ROUTE}/callback`;
 export const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 export const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
