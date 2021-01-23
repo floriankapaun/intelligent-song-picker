3d7c649329ad1314e561928ac33209168df1783f
Florian Kapaun <hello@florian-kapaun.de>
2021-01-23 19:14:45 +0100

    :sparkles: server static gzipped files

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import history from 'connect-history-api-fallback';
 import cookieParser from 'cookie-parser';
+import gzipStatic from 'connect-gzip-static';
 
 import { PORT, API_ROUTE, CLIENT_DIR } from './config.js';
 import router from './router/index.js';
@@ -12,7 +13,7 @@ app.use(cors())
     .use(cookieParser())
     .use(API_ROUTE, router)
     .use(history()) // Enable historyMode for direct access to vue routes
-    .use(express.static(CLIENT_DIR));
+    .use(gzipStatic(CLIENT_DIR));
 
 app.listen(PORT, () => {
     console.log(`Express server listening at http://localhost:${PORT}`);
