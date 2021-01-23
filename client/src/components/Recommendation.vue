6b802deff12ec105bbcff5fe2f5c69a7677825de
Florian Kapaun <hello@florian-kapaun.de>
2021-01-23 22:08:27 +0100

    :children_crossing: added loading animation to recommendation screen

--- client/src/components/Recommendation.vue
+++ client/src/components/Recommendation.vue
@@ -3,6 +3,16 @@
         <article v-if="selfie && selfie.img && selfie.url" class="fullscreen-background_wrapper transition-in_setup" :class="{ 'transition-in': imgLoaded }">
             <img :src="selfie.url" alt="The Selfie you took" @load="imgLoaded = true">
         </article>
+        <article v-if="isAnalyzingImage" class="fullscreen-background_wrapper p4">
+            <div class="loading-animation">
+                <div class="dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <p class="text-xs color-ink_light mt2">Analyzing mood & searching song</p>
+            </div>
+        </article>
         <player :recommendedTrack="recommendedTrack" :spotify="spotify" @deleteSelfie="$emit('deleteSelfie')"></player>
     </main>
 </template>
@@ -21,6 +31,7 @@ export default {
         return {
             imgLoaded: false,
             recommendedTrack: undefined,
+            isAnalyzingImage: false,
         };
     },
     components: {
@@ -29,6 +40,8 @@ export default {
     mounted() {
         // Setup listener for spotifyWorker
         spotifyWorker.worker.onmessage = (event) => {
+            // Hide loading animation again
+            this.isAnalyzingImage = false;
             const { error, result } = event.data;
             if (result && Object.keys(result).length > 0) {
                 this.recommendedTrack = result;
@@ -47,12 +60,13 @@ export default {
         };
         // Setup listener for imageWorker
         imageWorker.worker.onmessage = (event) => {
-            console.log(event.data);
             spotifyWorker.send({
                 accessToken: this.spotify.auth.accessToken,
                 parameters: event.data,
             });
         };
+        // Show loading animation
+        this.isAnalyzingImage = true;
         // Send the received selfie to the imageWorker to start the processing pipeline
         imageWorker.send(this.selfie.img);
     },
