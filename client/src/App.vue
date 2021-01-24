7290a1491e9f9b236311b1fdcf5c9371cecaae26
Florian Kapaun <hello@florian-kapaun.de>
2021-01-24 14:26:46 +0100

    :sparkles: implemented cookie consent and matomo tracking

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,22 @@
 <template>
     <component-header />
-    <router-view />
+    <router-view @openCookieConsent="openCookieConsent" />
+    <component-cookies ref="cookieConsent" />
 </template>
 
 <script>
 import ComponentHeader from '@/components/Header.vue';
+import ComponentCookies from '@/components/Cookies.vue';
 
 export default {
     components: {
         ComponentHeader,
+        ComponentCookies,
     },
+    methods: {
+        openCookieConsent() {
+            this.$refs.cookieConsent.openCookieModal();
+        },
+    }
 }
 </script>
--- client/src/views/DataPrivacy.vue
+++ client/src/views/DataPrivacy.vue
@@ -1,5 +1,16 @@
 <template>
   <main class="w-limited p4">
-    <h1>Data Privacy</h1>
+    <h1 class="mb4">Data Privacy</h1>
+    <p>Open the <router-link to="" @click.prevent="openCookieConsent">cookie-consent</router-link>.</p>
   </main>
 </template>
+
+<script>
+  export default {
+    methods: {
+      openCookieConsent() {
+        this.$emit('openCookieConsent');
+      },
+    },
+  }
+</script>
